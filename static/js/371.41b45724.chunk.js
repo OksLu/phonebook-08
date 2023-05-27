107775d1d3c3e2bcab491f0c6bad0a7005573c1c
OksLu <112413121+OksLu@users.noreply.github.com>
2023-05-27 21:37:42 +0000

    Deploying to gh-pages from @ OksLu/phonebook-08@b8fedae4486d149c4bb16f62c63f8dff90b3740b 🚀

---
+++ static/js/371.41b45724.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[371],{5655:function(e,t,a){a.r(t),a.d(t,{default:function(){return i}});var o="HomePage_section__IbGu2",s="HomePage_title__G4sQi",c=a.p+"static/media/hands.28a7aff8a5e0a7c76692.jpg",n=a(184),i=function(){return(0,n.jsxs)("section",{className:o,children:[(0,n.jsx)("h1",{className:s,children:"Welcome to Simple Phonebook!"}),(0,n.jsx)("img",{src:c,alt:"phonebook",width:"700",height:"460"})]})}}}]);
+//# sourceMappingURL=371.41b45724.chunk.js.map
